be97e0aee3d6e1e65cd3ee436c3f13de1e01c98b
zakirali05 <109236535+zakirali05@users.noreply.github.com>
2023-08-11 20:28:50 +0530

    second commit

---
+++ components/loader.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+
+const Loader = () => {
+  return (
+    <div className="h-full flex flex-col gp-y-4 items-center justify-center">
+        <div className="w-10 h-10 relative animate-spin">
+            <Image
+            alt="Logo"
+            fill
+            src="/logo.png"
+            />
+        </div>
+        <p className="text-sm text-muted-foreground">MyAI is thinking...</p>
+    </div>
+  )
+}
+
+export default Loader
